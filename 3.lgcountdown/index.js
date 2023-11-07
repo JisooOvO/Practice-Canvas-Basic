d39ec63568ca9afc5af7dbbe860c4b9b2003a8bd
fdking1887 <fdking1887@naver.com>
2023-11-07 20:07:36 +0900

    add lgcountdown, confetti

--- 3.lgcountdown/index.js
+++ 3.lgcountdown/index.js
@@ -20,7 +20,7 @@ function init() {
 }
 
 function createRing(){
-    const PARTICLE_NUM =1
+    const PARTICLE_NUM = 800
     for (let i=0; i<PARTICLE_NUM ; i++){
         particles.push(new Particle())
     }
@@ -36,11 +36,21 @@ function render(){
         delta = now - then
     
         if(delta < interval) return
-    
-        particles.forEach((particle,index)=>{
-            particle.update()
-            particle.draw(ctx)
-        })
+
+        //지우기
+        ctx.clearRect(0,0,canvasWidth,canvasHeight)
+        
+        //파티클 배열의 뒷부분을 우선순회하면
+        //Splice로 삭제되어 배열이 직접 수정되어도 배열 앞부분에 영향이 없음
+        //앞부분을 우선 순회할 경우 중간에 요소 삭제시 배열이 이동하여 뒷 요소에 영향
+        for(let i = particles.length - 1 ; i >= 0 ; i--){
+            particles[i].update()
+            particles[i].draw(ctx)
+
+            if(particles[i].opacity < 0){
+                particles.slice(i,1)
+            }
+        }
     
         then = now - (delta % interval)
     }
@@ -56,5 +66,41 @@ window.addEventListener('load',()=>{
 window.addEventListener('resize',init)
 
 window.addEventListener('click',()=>{
-    createRing()
+    const texts = document.querySelectorAll('span')
+
+    const countDownOption = {
+        opacity : 1,
+        scale : 1,
+        duration : 0.4,
+        ease : 'Power4.easeOut'
+    }
+
+    //gsap 라이브러리
+    gsap.fromTo(texts[0], {opacity:0, scale:5}, {
+        ...countDownOption
+    })
+
+    gsap.fromTo(texts[1], {opacity:0, scale:5},{
+        ...countDownOption,
+        delay : 1,
+        onStart : () => texts[0].style.opacity = 0
+    })
+
+    gsap.fromTo(texts[2], {opacity:0, scale:5},{
+        ...countDownOption,
+        delay : 2,
+        onStart : () => texts[1].style.opacity = 0
+    })
+
+    const ringImg = document.querySelector('#ring')
+
+    gsap.fromTo(ringImg, {opacity : 1}, {
+        opacity : 0,
+        duration : 1,
+        delay : 3,
+        onStart : () => {
+            createRing()
+            texts[2].style.opacity = 0
+        }
+    })
 })
--- 3.lgcountdown/js/Particle.js
+++ 3.lgcountdown/js/Particle.js
@@ -1,15 +1,37 @@
+import { randomNumBetween } from "./utils.js"
+
 export default class Particle {
     constructor(){
-        this.x = innerWidth / 2
-        this.y = innerHeight / 2
+        this.r = innerHeight / 4
+        this.angle = randomNumBetween(0,360)
+        this.rAlpha = randomNumBetween(0,5)
+        this.angleAlpha = randomNumBetween(1,2)
+        this.rFriction = randomNumBetween(0.95,1.01)
+        this.angleFriction = randomNumBetween(0.97,0.99)
+        this.opacity = randomNumBetween(0.2,1)
     }
 
-    update(){}
+    update(){
+        // 원 그리기 
+        //x = x1 + rcos(∅)
+        //y = y1 + rsin(∅)
+        this.rAlpha *= this.rFriction
+        this.angleAlpha *= this.angleFriction
+        // 원 수직/수평 힘    
+        this.r += this.rAlpha
+        this.angle += this.angleAlpha
+        
+        this.x = innerWidth / 2 + this.r * Math.cos(Math.PI / 180 * this.angle)
+        this.y = innerHeight / 2 + this.r * Math.sin(Math.PI / 180 * this.angle)
+
+        this.opacity -= 0.003
+    }
 
     draw(ctx){
         ctx.beginPath()
         ctx.arc(this.x, this.y, 1, 0, Math.PI * 2)
-        ctx.fillStyle = '#ffffff'
+        ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`
+        ctx.fill()
         ctx.closePath()
     }
 }
